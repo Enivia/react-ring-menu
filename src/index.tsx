a1779763cc0820ee00814cdecb24644143150631
Enivia <aqingcc@outlook.com>
2022-06-23 11:40:14 +0800

    init project

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import './index.less';
+
+// Delete me
+export const Thing = () => {
+  return <div className="container">the snozzberries taste like snozzberries</div>;
+};
